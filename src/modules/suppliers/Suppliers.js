895bcb1912e8152d4a751102bd9bc10fac5dc768
wHitedeVil4561 <deepak89484561@gmail.com>
2023-01-22 20:42:25 +0530

    supplier-routing

---
+++ src/modules/suppliers/Suppliers.js
@@ -0,0 +1,7 @@
+import { Outlet } from "react-router-dom";
+
+const SupplierLayout = () => {
+  return <Outlet />;
+};
+
+export default SupplierLayout;
--- src/modules/suppliers/components/add-stock/AddStock.js
+++ src/modules/suppliers/components/add-stock/AddStock.js
@@ -1,5 +1,5 @@
-const AddStockk = () => {
+const AddStock = () => {
   return <h1>Add Stock screen</h1>;
 };
 
-export default AddStockk;
+export default AddStock;
